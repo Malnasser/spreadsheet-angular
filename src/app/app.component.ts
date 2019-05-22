9202dbf1571cb23cc8ca22ebbf74afe7ccf649d3
mhalnasser <alnasser.nas@gmail.com>
2019-05-22 08:39:24 -0500

    data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,38 @@
 import { Component } from '@angular/core';
 
+export interface PeriodicElement {
+  A: string;
+  B: string;
+  C: string;
+  D: string;
+  E: string;
+  F: string;
+  G: string;
+  H: string;
+  I: string;
+  J: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {A: undefined, B: undefined, C: undefined, D: undefined, E: undefined, F: undefined, G: undefined, H: undefined, I: undefined, J: undefined},
+  {A: undefined, B: undefined, C: undefined, D: undefined, E: undefined, F: undefined, G: undefined, H: undefined, I: undefined, J: undefined},
+  {A: undefined, B: undefined, C: undefined, D: undefined, E: undefined, F: undefined, G: undefined, H: undefined, I: undefined, J: undefined},
+  {A: undefined, B: undefined, C: undefined, D: undefined, E: undefined, F: undefined, G: undefined, H: undefined, I: undefined, J: undefined},
+  {A: undefined, B: undefined, C: undefined, D: undefined, E: undefined, F: undefined, G: undefined, H: undefined, I: undefined, J: undefined},
+  {A: undefined, B: undefined, C: undefined, D: undefined, E: undefined, F: undefined, G: undefined, H: undefined, I: undefined, J: undefined},
+  {A: undefined, B: undefined, C: undefined, D: undefined, E: undefined, F: undefined, G: undefined, H: undefined, I: undefined, J: undefined},
+  {A: undefined, B: undefined, C: undefined, D: undefined, E: undefined, F: undefined, G: undefined, H: undefined, I: undefined, J: undefined},
+  {A: undefined, B: undefined, C: undefined, D: undefined, E: undefined, F: undefined, G: undefined, H: undefined, I: undefined, J: undefined},
+  {A: undefined, B: undefined, C: undefined, D: undefined, E: undefined, F: undefined, G: undefined, H: undefined, I: undefined, J: undefined},
+  {A: undefined, B: undefined, C: undefined, D: undefined, E: undefined, F: undefined, G: undefined, H: undefined, I: undefined, J: undefined},
+  {A: undefined, B: undefined, C: undefined, D: undefined, E: undefined, F: undefined, G: undefined, H: undefined, I: undefined, J: undefined},
+  {A: undefined, B: undefined, C: undefined, D: undefined, E: undefined, F: undefined, G: undefined, H: undefined, I: undefined, J: undefined},
+  {A: undefined, B: undefined, C: undefined, D: undefined, E: undefined, F: undefined, G: undefined, H: undefined, I: undefined, J: undefined},
+  {A: undefined, B: undefined, C: undefined, D: undefined, E: undefined, F: undefined, G: undefined, H: undefined, I: undefined, J: undefined},
+  {A: undefined, B: undefined, C: undefined, D: undefined, E: undefined, F: undefined, G: undefined, H: undefined, I: undefined, J: undefined},
+  {A: undefined, B: undefined, C: undefined, D: undefined, E: undefined, F: undefined, G: undefined, H: undefined, I: undefined, J: undefined},
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
